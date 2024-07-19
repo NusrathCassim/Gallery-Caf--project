225f8a0e6c839ec628ad08d2aa520cd1909e0a14
Nusrath cassim <nusrathcassim1@gmail.com>
2024-07-20 00:19:08 +0530

    admin-template

--- main-folder/admin/template_admin.php
+++ main-folder/admin/template_admin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <!-- Boxicons CSS -->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="template.css">
+    <link rel="stylesheet" href="template-admin.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
 </head>
@@ -18,12 +18,24 @@
         </div>
         
             <ul class="nav-links">
-                <li><a href="/main-folder/admin/add-menu/add-menu.php">
-                        <i class='bx bx-sitemap'></i>
-                        <span class="link_name">
-                        Add Menu
-                        </span>
-                </a></li>
+                <li>
+               
+                    <div class="icon-link">
+                        <a href="/main-folder/admin/add-menu/add-menu.php">
+                            <i class='bx bx-sitemap'></i>
+                            <span class="link_name">
+                                Menu
+                            </span>
+                        </a><i class="bx bx-chevron-down arrow"></i>
+                        
+                    </div>
+                    <ul class="sub-menu">
+                        <li><a class="link_name" href=""> Manage</a></li>
+                        <li><a href="/main-folder/admin/add-menu/add-menu.php">Add Menu</a></li>
+                        <li><a href="/main-folder/admin/add-menu/edit-menu.php">Edit Menu</a></li>
+                    </ul>
+                
+                </li>
                 <li><a href="">
                     <i class='bx bxs-check-circle'></i>
                     <span class="link_name">
@@ -43,22 +55,33 @@
                     
                 </li>
                 <li><a href="#">
-                    <i class='bx bx-cog'></i>
-                        <span class="link_name">
-                         Add Special Events
-                        </span>
-                </a></li>
+                    <div class="icon-link">
+                        <a href="/main-folder/admin/add-specialEvents/add_specialEvents.php">
+                            <i class='bx bx-cog'></i>
+                            <span class="link_name">
+                                Special Events
+                            </span>
+                        </a><i class="bx bx-chevron-down arrow"></i>
+                        
+                    </div>
+                    <ul class="sub-menu">
+                        <li><a class="link_name" href=""> Manage</a></li>
+                        <li><a href="/main-folder/admin/add-specialEvents/add_specialEvents.php">Add Events</a></li>
+                        <li><a href="/main-folder/admin/add-specialEvents/edit_event.php">Edit Events</a></li>
+                    </ul>
+            
+                </li>
                 <li><a href="#">
                     <i class='bx bx-grid-alt'></i>
                         <span class="link_name">
                         Customer Reviews
                         </span>
                 </a></li>
-                <li><a href="#">
+                <li><a href="/main-folder/SignUp/logout.php">
                     <i class='bx bx-grid-alt'></i>
-                        <span class="link_name">
-                            Logout
-                        </span>
+                    <span class="link_name">
+                        Logout
+                    </span>
                 </a></li>
 
         </ul>    
